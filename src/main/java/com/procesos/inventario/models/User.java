b512061bcebc92b7a5e3c1cb3fb020eecc82fb73
Darlyn Fuentes <dmfuentesr@ufpso.edu.co>
2023-03-21 20:57:30 -0500

    agregar servicicios y endpoin de consultar todos los usuarios

--- src/main/java/com/procesos/inventario/models/User.java
+++ src/main/java/com/procesos/inventario/models/User.java
@@ -15,8 +15,8 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    private String address;
     private String email;
     private String password;
+    private String address;
     private Date birthday;
 }
